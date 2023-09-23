23f737f4c25df7a059d22a47634c452e9e2cd175
Abhinav Sinha <abhinavvsinhaa@gmail.com>
2023-09-23 18:49:16 +0530

    modified enum for hierarchy and updated user interface for token

---
+++ src/types/user.types.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export enum Roles {
+    TEAM_MEMBER,
+    TEAM_LEADER,
+    ADMIN,
+    SUPER_ADMIN,
+    FIRST_ADMIN
+}
+
+export type keyRoles = keyof Roles
+
+export interface IUser {
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    role: Roles,
+    agency?: mongoose.Types.ObjectId,
+    token?: string
+}
