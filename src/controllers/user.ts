7d12093f029e79b88610c032e641535e8ee35012
Abhinav Sinha <abhinavvsinhaa@gmail.com>
2023-09-23 18:47:39 +0530

    token signing and decoding

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,14 +1,36 @@
 import { Request, Response } from "express";
 import { Error, Success } from "../types/response.types";
-import { IUser } from "../types/user.types";
 import { StatusCodes } from "http-status-codes";
 import { User } from "../models/user";
+import jwt, { Secret } from 'jsonwebtoken'
+import { IToken } from "../types/token.types";
+import dotenv from 'dotenv';
+import { ICustomRequest } from "../types/customrequest.types";
+dotenv.config({ path: '../.env' })
+
+const secret: Secret = process.env.SECRET!;
 
 /**
  * Create user 
  */
-export const createUser = async (user: IUser, req: Request, res: Response) => {
+export const createUser = async (req: Request, res: Response) => {
     try {
+        const r = req as ICustomRequest;
+        const decoded = r.decoded as IToken;
+
+        // finding user with decoded id from token
+        const user = await User.findById(decoded.id);
+
+        if (!user) {
+            const err: Error = {
+                message: 'no user exists with the id',
+                code: StatusCodes.BAD_REQUEST,
+                err: 'NA'
+            }
+            res.status(err.code!).send(err);
+            return;
+        }
+
         const { firstName, lastName, email, password, role, agency } = req.body;
 
         // if the role of user is lesser than requested role to be created, throw error
@@ -51,6 +73,17 @@ export const createUser = async (user: IUser, req: Request, res: Response) => {
             role,
             agency
         });
+
+        const payload: IToken = { id: newUser._id }
+
+        // creating new token
+        const newUserToken = jwt.sign(payload, secret, {
+            expiresIn: '24h'
+        })
+
+        // setting the token for new user
+        newUser.token = newUserToken;
+
         await newUser.save();
 
         const response: Success = {
