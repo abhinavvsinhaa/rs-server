30d80388ad3010549c20b38fdc42679c85d46979
Abhinav Sinha <abhinavvsinhaa@gmail.com>
2023-09-24 11:51:13 +0530

    added namespace for agency route & refactored

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -2,11 +2,9 @@ import express from 'express'
 import {createUser, loginUser, me} from '../controllers';
 import {auth} from "../middlewares/auth";
 
-const authRouter = express.Router();
+export const authRouter = express.Router();
 
 authRouter
     .post('/register',auth, createUser)
     .post('/login', loginUser)
     .get('/me',auth,me)
-
-export default authRouter;
--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import authRouter from "./auth.route";
+import { agencyRouter } from "./agency.route";
 
 const router = express.Router();
 
 router.use('/auth',authRouter)
+router.use('/agency', agencyRouter);
 
 export default router
