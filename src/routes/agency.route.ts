c5ff4e9e4693af14ab3942f799394dab020255c6
Abhinav Sinha <abhinavvsinhaa@gmail.com>
2023-09-24 11:48:31 +0530

    / route on get all agencies

--- src/routes/agency.route.ts
+++ src/routes/agency.route.ts
@@ -1,6 +1,8 @@
 import express from 'express'
-import {registerAgency} from '../controllers';
+import {registerAgency, getAllAgencies} from '../controllers';
+import {auth} from "../middlewares/auth";
 export const agencyRouter = express.Router();
 
 agencyRouter
+    .get('/', auth, getAllAgencies)
     .post('/register', registerAgency)
